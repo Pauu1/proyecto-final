e92b9d07abd6e19d42a8b0358124578fbe962465
Paulinatello <1720110731@utectulancingo.edu.mx>
2022-04-22 22:59:53 +0000

    firebase token

---
+++ firebase_config.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyAOvID6GHW66QnQBVMUVy60wXId1uFFtZQ",
+  "authDomain": "awi4-30361.firebaseapp.com",
+  "databaseURL": "https://awi4-30361-default-rtdb.firebaseio.com",
+  "projectId": "awi4-30361",
+  "storageBucket": "awi4-30361.appspot.com",
+  "messagingSenderId": "173612988823",
+  "appId": "1:173612988823:web:2ffec52f4b10620e842cd2",
+  "measurementId": "G-JN6SXPX8BP"
+}
