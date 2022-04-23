2d759ec8cc545eea73dd752458004bc52c0ec8a3
Paulinatello <1720110731@utectulancingo.edu.mx>
2022-04-23 00:38:35 +0000

    app.py con urls

---
+++ app.py
@@ -0,0 +1,23 @@
+import web
+import pyrebase 
+import firebase_config as token
+import json
+
+urls = (
+    '/', 'mvc.controllers.public.inicio.Inicio',  
+    '/user_list', 'mvc.controllers.admin.user_list.User_list',
+    '/bienvenida_administrador','mvc.controllers.admin.bienvenida_administrador.Bienvenida_administrador',
+    '/sensor_temperatura','mvc.controllers.admin.sensor_temperatura.Sensor_temperatura', 
+    '/user_view/(.*)','mvc.controllers.admin.user_view.User_view',
+    '/sensor_temperatura','mvc.controllers.operador.sensor_temperatura.Sensor_temperatura',
+    '/login', 'mvc.controllers.public.login.Login',
+     '/registro','mvc.controllers.admin.registro.Registro',
+    '/bienvenida_usuario','mvc.controllers.operador.bienvenida_usuario.Bienvenida_usuario',
+    '/r_contraseña', 'mvc.controllers.public.r_contraseña.R_contraseña',   
+)
+app = web.application(urls, globals())
+
+
+if __name__ == "__main__":
+    web.config.debug = True 
+    app.run()
