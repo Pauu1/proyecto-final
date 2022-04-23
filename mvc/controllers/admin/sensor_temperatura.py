1098cb566931db985836f600256703339014298a
Paulinatello <1720110731@utectulancingo.edu.mx>
2022-04-23 00:30:56 +0000

    Sensor_temperatura administrador

---
+++ mvc/controllers/admin/sensor_temperatura.py
@@ -0,0 +1,17 @@
+import web
+import pyrebase 
+import firebase_config as token
+import json
+
+
+render = web.template.render('mvc/view/admin', base="layout")
+
+
+class Sensor_temperatura:
+      def GET(self):
+        try:
+            firebase = pyrebase.initialize_app(token.firebaseConfig) 
+            db = firebase.database() 
+            return render.sensor_temperatura() 
+        except Exception as error: 
+            print("Error sensor_temperatura.GET: {}".format(error))
